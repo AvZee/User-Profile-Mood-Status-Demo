b0ae9311e7aa719e7e7ad0f06359e95db2dbd363
AvZee <anton.v.zhuk@gmail.com>
2026-05-03 18:18:34 -0700

    Updated index.ts port declaration

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { app } from "./app";
 import { logger } from "./lib/logger";
 
-const port = Number(process.env.PORT) || 3000;
+const port = Number(process.env.PORT ?? 3000);
 
 logger.info({ port }, "Server starting");
 
